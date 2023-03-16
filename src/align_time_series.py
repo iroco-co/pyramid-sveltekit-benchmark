e66023ea5dae8ad798bec1eb06d71640b9dc95cd
Bruno Thomas <bruno@barreverte.fr>
2023-03-16 10:02:15 +0100

    fix: merge shift time series and rewrite of new file

--- src/align_time_series.py
+++ src/align_time_series.py
@@ -1,6 +1,6 @@
 import argparse
 import csv
-from datetime import timedelta, datetime
+from datetime import datetime
 from os.path import basename, join, dirname
 from time import strptime, struct_time, mktime
 
@@ -13,29 +13,24 @@ def first_non_null_value(csv_data, time_column, value_column, time_format) -> st
             return strptime(line[time_column], time_format)
 
 
-def shift_time_series(csv_data, time_column: int, delta: timedelta, time_format: str):
-    for line in csv_data:
-        time_value = strptime(line[time_column], time_format)
-        shifted_value = datetime.fromtimestamp(mktime(time_value)) + delta
-        yield shifted_value.strftime(time_format)
-
-
 def process(arguments: argparse.Namespace):
     with open(arguments.filename1) as f1, open(arguments.filename2) as f2:
         csv1 = csv.reader(f1, delimiter=arguments.delimiter)
         csv2 = csv.reader(f2, delimiter=arguments.delimiter)
         t1 = first_non_null_value(csv1, arguments.time1-1, arguments.column1-1, arguments.format)
         t2 = first_non_null_value(csv2, arguments.time2-1, arguments.column2-1, arguments.format)
-        t2_shifted = shift_time_series(csv2, arguments.time1-1,
-                                       datetime.fromtimestamp(mktime(t1)) -
-                                       datetime.fromtimestamp(mktime(t2)),
-                                       arguments.format)
-        with open(arguments.filename2) as f2bis,\
-             open(join(dirname(arguments.filename2), f'new-{basename(arguments.filename2)}'), 'w') as f2new:
-            csv_writer = csv.writer(f2new, delimiter=arguments.delimiter)
-            csv2 = csv.reader(f2bis, delimiter=arguments.delimiter)
-            for t, csv_line in zip(t2_shifted, csv2):
-                csv_writer.writerow([t] + csv_line[1:])
+
+    delta = datetime.fromtimestamp(mktime(t1)) - datetime.fromtimestamp(mktime(t2))
+
+    with open(arguments.filename2) as f2bis,\
+         open(join(dirname(arguments.filename2), f'new-{basename(arguments.filename2)}'), 'w') as f2new:
+        csv_writer = csv.writer(f2new, delimiter=arguments.delimiter)
+        csv2 = csv.reader(f2bis, delimiter=arguments.delimiter)
+        for csv_line in csv2:
+            time_value = strptime(csv_line[arguments.time1-1], arguments.format)
+            shifted_value = datetime.fromtimestamp(mktime(time_value)) + delta
+            time_str = shifted_value.strftime(arguments.format)
+            csv_writer.writerow([time_str] + csv_line[1:])
 
 
 if __name__ == '__main__':
